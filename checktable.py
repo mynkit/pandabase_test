c308ea026778c88511ee3e7edef0f3957977b475
mynkit <dcm5124289@gmail.com>
2020-02-12 22:50:37 +0900

    autopep8

--- checktable.py
+++ checktable.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 if __name__ == '__main__':
-    con = create_engine('mysql+mysqlconnector://root:password@localhost/sample_db')
+    con = create_engine(
+        'mysql+mysqlconnector://root:password@localhost/sample_db')
     users = pd.read_sql('SELECT * FROM users;', con)
     print(users.head())
